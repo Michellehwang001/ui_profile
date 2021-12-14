6d00a94cdb1da0b1d8fe5480f3ce5202ef165376
Hwang Hyejung <jimin_ma@naver.com>
2021-12-15 06:06:06 +0900

    Header완성

--- lib/components/profile_header.dart
+++ lib/components/profile_header.dart
@@ -7,19 +7,50 @@ class ProfileHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const SizedBox(width: 20,),
+        const SizedBox(
+          width: 20,
+        ),
         _buildHeaderAvatar(),
-        const SizedBox(width: 20,),
+        const SizedBox(
+          width: 20,
+        ),
         _buildHeaderProfile(),
       ],
     );
   }
 
   Widget _buildHeaderAvatar() {
-    return SizedBox();
+    return const SizedBox(
+      width: 100,
+      height: 100,
+      child: CircleAvatar(
+        backgroundImage: AssetImage('assets/profile.png'),
+      ),
+    );
   }
 
   Widget _buildHeaderProfile() {
-    return SizedBox();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          'Michelle Hwang',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        SizedBox(height: 3,),
+        Text(
+          '지니박스CEO',
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 3,),
+        Text(
+          'Flutter Programmer',
+          style: TextStyle(fontSize: 15),
+        ),
+      ],
+    );
   }
 }
